8375e8511c36c7505a6c160e8d51046a4346f05f
AdvaitT17 <advait1702@gmail.com>
2017-10-28 16:42:59 +0530

    Update service-installer.sh

--- scripts/service-installer.sh
+++ scripts/service-installer.sh
@@ -2,8 +2,7 @@
 #
 # Install systemd service files for running on startup.
 #
-# Copyright 2017 Google Inc.
-#
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
